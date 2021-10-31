5ed443cfa62aff86ce2d661859f15293f0323806
Mikhail Safir <safir@thenoco.co>
2021-10-31 15:37:36 +0300

    Initial Commit

---
+++ No.Emotion/No_EmotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  No_EmotionApp.swift
+//  No.Emotion
+//
+//  Created by Michael Safir on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct No_EmotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
